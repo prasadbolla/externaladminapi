3ccad3936f475414ab4a32d473c577bd96003c3f
Prasad Bolla <prasad.bolla1@ibm.com>
2020-01-17 00:31:29 -0500

    initial changes

---
+++ src/main/java/com/api/external/admin/ExternaladminapiApplication.java
@@ -0,0 +1,13 @@
+package com.api.external.admin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExternaladminapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExternaladminapiApplication.class, args);
+	}
+
+}
